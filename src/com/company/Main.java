b3c9e6098b57e8a9fd8c130310762f0c4857659f
diljasigurdar <diljas19@ru.is>
2020-08-26 16:23:08 +0000

    bæta í lista komið

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,43 @@
 package com.company;
 
+import edu.princeton.cs.algs4.BinaryIn;
+import edu.princeton.cs.algs4.BinaryInsertion;
+import edu.princeton.cs.algs4.Quick;
 import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        int N = 100;
+        int N = 10;
         StdOut.println(N);
         N = couponCollectorTest(N);
         StdOut.println(N);
     }
 
     public static int couponCollectorTest(int N){
+        int counter = 0;
         //Býr til random tölur 0-N þangað til allar tölur hafa komið upp OG prentar út hversu margar tölur í heildina voru búnar til í ferlinu.
         N = N + 1;
+        String[] test;
+        test = new String[N];
+        for (int i = 0; i<N; i++){
+            int x = getRandomNumber(N);
+            counter++;
+            test[i] = String.valueOf(x);
+            StdOut.println(test[i]);
+        }
+        for (int i = 0; i<N; i++){
+            StdOut.println(test[i]);
+        }
+        StdOut.println("after sorting");
+        StdOut.println(counter);
         return N;
+    }
+
+    public static int getRandomNumber(int N){
+        Random rand = new Random();
+        return rand.nextInt(N);
     }
 
     void CouponCollectorStats(int N, int T){
