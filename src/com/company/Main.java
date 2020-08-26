25c285dd50643bdb1233e13ced2f04c058e7383d
sunnarun <sunnat16@ru.is>
2020-08-26 16:23:20 +0000

    Random method ready

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,18 +2,24 @@ package com.company;
 
 import edu.princeton.cs.algs4.StdOut;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         int N = 100;
-        StdOut.println(N);
+        //StdOut.println(N);
         N = couponCollectorTest(N);
-        StdOut.println(N);
+        //StdOut.println(N);
     }
 
     public static int couponCollectorTest(int N){
         //Býr til random tölur 0-N þangað til allar tölur hafa komið upp OG prentar út hversu margar tölur í heildina voru búnar til í ferlinu.
-        N = N + 1;
         return N;
+    }
+
+    public static int getRandomNumber(int N){
+        Random rand = new Random();
+        return rand.nextInt(N);
     }
 
     void CouponCollectorStats(int N, int T){
